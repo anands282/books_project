eed91cdeb127392e79d3be9f78aaec04be58e85d
Anand Satheesh <anands282@gmail.com>
2024-06-12 22:07:46 +0530

    updated books get service

---
+++ books.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+books = [
+    {'title': 'Book1', 'author': 'Author1', 'category': "science"},
+    {'title': 'Book2', 'author': 'Author2', 'category': "fiction"},
+    {'title': 'Book3', 'author': 'Author3', 'category': "history"},
+    {'title': 'Book4', 'author': 'Author4', 'category': "current_affairs"},
+    {'title': 'Book5', 'author': 'Author5', 'category': "science"},
+    {'title': 'Book6', 'author': 'Author6', 'category': "fiction"},
+]
+@app.get("/books")
+async def read_all_books():
+    return books
